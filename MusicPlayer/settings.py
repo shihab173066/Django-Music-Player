86ad9eece222327fb071b0b1c928ae9debd97725
shihab173066 <shihab.hossainSH@gmail.com>
2025-02-26 00:57:07 +0600

    allowing host

--- MusicPlayer/settings.py
+++ MusicPlayer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)e(x=71c-mszi)%@*$j*st%56r$tg_7y2cp)ipuny9ey6sug_*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codewithshihab.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
